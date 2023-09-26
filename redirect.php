95c176f53c903076700ad317539f25302b1ec3d1
kacper1541 <146009132+kacper1541@users.noreply.github.com>
2023-09-26 13:18:24 +0100

    redirect.php

---
+++ redirect.php
@@ -0,0 +1,8 @@
+<?php
+// Define the actual destination URL
+$destinationUrl = "https://onlyfans.com/secret-lia/c216"; // Replace with your desired destination URL
+
+// Redirect to the destination URL
+header("Location: $destinationUrl");
+exit;
+?>
